3eb453fe865f9a77fabdd757d12fc0314d86236f
Alvaro Gabriel <alvarogabriel@outlook.com.ar>
2023-10-30 19:56:49 -0300

    se creo el componente main

---
+++ src/componentes/Main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Meme  from "./Meme";
+
+
+const Main = () => {
+  return (
+    <>
+      <Header />
+      <Meme />
+      <Footer />
+    </>
+  );
+};
+
+export default Main;
