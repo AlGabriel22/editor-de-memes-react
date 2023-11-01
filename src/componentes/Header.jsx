ba79c40c45b8c98c0a4853537e1fd7c7f80de701
Alvaro Gabriel <alvarogabriel@outlook.com.ar>
2023-11-01 02:46:16 -0300

    se cambiaron clases de materializecss

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -3,9 +3,16 @@ import '../hojas-de-estilo/header.css';
 
 const Header = () => {
 	return (
-		<nav className='teal'>
+		<nav className='blue darken-3'>
 			{/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-			<p className='brand-logo'>Generador de Memes</p>
+			<div className='brand-logo'>
+				<img
+					className='img-header'
+					src='http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png'
+					alt='troll face'
+				/>
+				<p className='logo'>Generador de Memes</p>
+			</div>
 		</nav>
 	);
 };
