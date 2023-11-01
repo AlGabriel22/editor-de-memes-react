757812662d5c17af3cfb398257ed473fa71a7da7
Alvaro Gabriel <alvarogabriel@outlook.com.ar>
2023-11-01 20:25:46 -0300

    se cambio la clase del nav

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -3,7 +3,7 @@ import '../hojas-de-estilo/header.css';
 
 const Header = () => {
 	return (
-		<nav className='blue darken-3'>
+		<nav>
 			{/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
 			<div className='brand-logo'>
 				<img
@@ -11,7 +11,7 @@ const Header = () => {
 					src='http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png'
 					alt='troll face'
 				/>
-				<p className='logo'>Generador de Memes</p>
+				<p className='logo'>Editor de Memes</p>
 			</div>
 		</nav>
 	);
